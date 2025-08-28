eccd326723537a47124ac7d7f95d532b93e9cf3f
Maciej Majorczyk <70149135+MaciejDotDev@users.noreply.github.com>
2025-08-28 12:00:52 +0100

    Attempting to fix blank page

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,4 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-})
+export default defineConfig({ base: '/maciejm.dev/', plugins: [react()] })
